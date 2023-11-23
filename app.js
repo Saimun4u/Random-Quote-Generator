8019e477cad274950978aec7799c751b56f6d0e8
Saimun Hassan <123021229+saimun4all@users.noreply.github.com>
2023-11-22 20:56:58 -0500

    Added try catch functionality for the generator for showing message if server is slow

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const showQuote = document.getElementById('quote');
 const author = document.getElementById('author');
 
 async function getQuote() {
-  const response = await fetch('https://api.quotable.io/random');
-  const quote = await response.json();
+  try {
+    showQuote.textContent = 'Updating...';
+    btn.disabled = true;
+    btn.innerText = 'Loading...';
+    const response = await fetch('https://api.quotable.io/random');
+    const quote = await response.json();
+
+    btn.disabled = false;
+    btn.innerText = 'Get me a quote';
+
+    showQuote.textContent = quote.content;
+    author.textContent = `- ${quote.author}`;
+  } catch (error) {
+    showQuote.style.color = 'red';
+    showQuote.textContent = 'Ohhh.. the site has hit a snag';
+    btn.disabled = false;
+    btn.innerText = 'GET QUOTE';
+  }
 
   console.log(quote.content);
   console.log(`- ${quote.author}`);
-
-  showQuote.textContent = quote.content;
 }
 
 btn.addEventListener('click', getQuote);
